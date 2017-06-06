4c996d028860dd0867e1ac89f4b51f3d07e5897a
AKS1996 <sushsilsharma.ss451@gmail.com>
2017-06-06 16:52:20 +0530

    Particle Filter converging to a desired heading direction

--- particle filter robot.py
+++ particle filter robot.py
@@ -5,27 +5,22 @@ landmarks = [[20.0, 20.0], [80.0, 80.0], [20.0, 80.0], [80.0, 20.0]]
 world_size = 100.0
 
 
-def Gaussian(mu, sigma, x):
-    # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
-    return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
-
-
 class robot:
     def __init__(self):
         self.x = random.random() * world_size
         self.y = random.random() * world_size
         self.orientation = random.random() * 2.0 * pi
-        self.forward_noise = 0.0
-        self.turn_noise = 0.0
-        self.sense_noise = 0.0
+        self.forward_noise = 0.0;
+        self.turn_noise = 0.0;
+        self.sense_noise = 0.0;
 
     def set(self, new_x, new_y, new_orientation):
         if new_x < 0 or new_x >= world_size:
-            raise (ValueError, "X coordinate out of bound")
+            raise ValueError, 'X coordinate out of bound'
         if new_y < 0 or new_y >= world_size:
-            raise (ValueError, 'Y coordinate out of bound')
+            raise ValueError, 'Y coordinate out of bound'
         if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise (ValueError, 'Orientation must be in [0..2pi]')
+            raise ValueError, 'Orientation must be in [0..2pi]'
         self.x = float(new_x)
         self.y = float(new_y)
         self.orientation = float(new_orientation)
@@ -33,21 +28,21 @@ class robot:
     def set_noise(self, new_f_noise, new_t_noise, new_s_noise):
         # makes it possible to change the noise parameters
         # this is often useful in particle filters
-        self.forward_noise = float(new_f_noise)
-        self.turn_noise = float(new_t_noise)
-        self.sense_noise = float(new_s_noise)
+        self.forward_noise = float(new_f_noise);
+        self.turn_noise = float(new_t_noise);
+        self.sense_noise = float(new_s_noise);
 
     def sense(self):
-        z = []
+        Z = []
         for i in range(len(landmarks)):
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
             dist += random.gauss(0.0, self.sense_noise)
-            z.append(dist)
-        return z
+            Z.append(dist)
+        return Z
 
     def move(self, turn, forward):
         if forward < 0:
-            raise (ValueError, 'Robot cant move backwards')
+            raise ValueError, 'Robot cant move backwards'
 
             # turn, and add randomness to the turning command
         orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
@@ -66,14 +61,19 @@ class robot:
         res.set_noise(self.forward_noise, self.turn_noise, self.sense_noise)
         return res
 
+    def Gaussian(self, mu, sigma, x):
+
+        # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
+        return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
+
     def measurement_prob(self, measurement):
 
         # calculates how likely a measurement should be
 
-        prob = 1.0
+        prob = 1.0;
         for i in range(len(landmarks)):
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
-            prob *= Gaussian(dist, self.sense_noise, measurement[i])
+            prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
         return prob
 
     def __repr__(self):
@@ -81,7 +81,7 @@ class robot:
 
 
 def eval(r, p):
-    sum = 0.0
+    sum = 0.0;
     for i in range(len(p)):  # calculate mean error
         dx = (p[i].x - r.x + (world_size / 2.0)) % world_size - (world_size / 2.0)
         dy = (p[i].y - r.y + (world_size / 2.0)) % world_size - (world_size / 2.0)
@@ -90,36 +90,51 @@ def eval(r, p):
     return sum / float(len(p))
 
 
-def main():
-    n = 1000
-    my_robot = robot()
-    z = my_robot.sense()
+# myrobot = robot()
+# myrobot.set_noise(5.0, 0.1, 5.0)
+# myrobot.set(30.0, 50.0, pi/2)
+# myrobot = myrobot.move(-pi/2, 15.0)
+# print myrobot.sense()
+# myrobot = myrobot.move(-pi/2, 10.0)
+# print myrobot.sense()
 
-    p = []
-    for i in range(n):
-        x = robot()
-        x.set_noise(0.05, 0.05, 5.0)
-        x = x.move(0.1, 5)
-        p.append(x)
+####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER/MODIFY CODE BELOW ####
+myrobot = robot()
+myrobot = myrobot.move(0.1, 5.0)
+Z = myrobot.sense()
+N = 1000
+T = 10  # Number of 'T'imes we'll do resampling
+# More iterations => Getting a common orientation
 
-    w = []  # weights
-    for i in range(n):
-        w.append(p[i].measurement_prob(z))
+p = []
+for i in range(N):
+    r = robot()
+    r.set_noise(0.05, 0.05, 5.0)
+    p.append(r)
 
-    s = sum(w)  # normalising weights
-    for i in range(n):
-        w[i] /= s
+for t in range(T):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
 
-    # Resampling via multiply random numbers with particle probability, select max of it
-    p_resampled = []
-    for i in range(n):
-        copy = w[:]
-        for j in range(n):
-            copy[j] *= random.uniform(0, 1)
-        p_resampled.append(p[copy.index(max(copy))])
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
 
-    # TODO  Resampling Wheel with O(n) instead O(n*log(n)) here
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
 
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
 
-if __name__ == '__main__':
-    main()
+    print eval(myrobot, p)
